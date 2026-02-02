261eed1f5a346b0fc44426b7774e5468d96b4c8e
ShumyloYana <zolotushko1@gmail.com>
2026-02-02 09:08:59 +0200

    Fixed header after login or registration user

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,26 @@
 import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import {  Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import Nav from '../Header/Nav/Nav';
 import AuthNav from '../Header/AuthNav/AuthNav';
 import UserNav from '../Header/UserNav/UserNav';
 import css from './Header.module.css';
+import LogOutBtn from './LogOutBtn/LogOutBtn';
 
 export default function Header() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, user } = useAuth();
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isTablet, setIsTablet] = useState(window.innerWidth >= 768 && window.innerWidth < 1280);
   const toggleMenu = () => setMenuOpen(prev => !prev);
 
+  
   useEffect(() => {
-  const handleResize = () => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+      setIsTablet(window.innerWidth >= 768 && window.innerWidth < 1280);
+      
     if (window.innerWidth >= 1280) {
       setMenuOpen(false);
     }
@@ -33,6 +40,7 @@ export default function Header() {
   return (
     <header className={css.header}>
       <div className={css.container}>
+        {/* логотип */}
         <Link to="/" className={css.logo}>
           {isHome ? (
             <svg width="106" height="28">
@@ -45,27 +53,71 @@ export default function Header() {
           )}
       </Link>
 
-        {/* планшет */}
-      <nav className={css.desktopNav}>
+        {/* компютерна версія тільки навігація */}
+        <nav className={css.desktopNav}>
       <Nav isHome={isHome}/>
-        {isAuthenticated ? <UserNav /> : <AuthNav />}
-        </nav>
-        
-        {/* компонент AuthNav на сторінці Логін та реєстрації на планшеті*/}
-        {!isAuthenticated && isAuthPage && (
-          <div className={css.tabletAuthNav}>
-            <AuthNav />
-          </div>
+        </nav>       
+      
+        {/* {лише аватарка в хедері на мобілці} */}
+        {isAuthenticated && isMobile && (
+          <Link to="/profile" className={css.mobileUser}>
+            <img
+              src={user?.avatar || '/default-avatar.png'}
+              alt="avatar"
+              className={css.avatar}
+            />
+          </Link>
         )}
 
-        {/* бургер меню на мобілці та планшеті */}
-      <button className={`${css.burgerBtn} ${burgerColorClass}`} onClick={toggleMenu} aria-label="Open menu">
-          <svg width="32" height="32">
-          <use href="/svg-sprite.svg#icon-menu-burger" />
-        </svg>
-        </button>        
+         {/* на планшеті в хедері лише ім'я та аватарка */}
+        {isAuthenticated && isTablet && (
+          <Link to="/profile" className={css.userInfo}>
+            <img
+              src={user?.avatar || '/default-avatar.png'}
+              alt="avatar"
+              className={css.avatar}
+            />
+            <p className={css.name}>{user?.name}</p>
+          </Link>
+        )}
+
+        
+        {/* {на  комп'ютері в хедері кнопка Логаут + аватарка + імя} */}
+{!isMobile && !isTablet && (
+  <>
+    {!isAuthenticated && isAuthPage && (
+      <div className={css.desktopAuthNav}>
+        <AuthNav />
+      </div>
+    )}
+
+    {isAuthenticated && (
+      <div className={css.desktopHeaderUser}>
+        <LogOutBtn />
+        <Link to="/profile" className={css.userInfo}>
+          <img
+            src={user?.avatar || '/default-avatar.png'}
+            alt="avatar"
+            className={css.avatar}
+          />
+          <p className={css.name}>{user?.name}</p>
+        </Link>
+      </div>
+    )}
+  </>
+)}
+
+        {/* бургер меню на мобілці та планшеті*/}
+        {(isMobile || isTablet) && (
+          <button className={`${css.burgerBtn} ${burgerColorClass}`} onClick={toggleMenu} aria-label="Open menu">
+            <svg width="32" height="32">
+              <use href="/svg-sprite.svg#icon-menu-burger" />
+            </svg>
+          </button>
+        )}  
       </div>
       
+      {/* контент бургер меню */}
       {menuOpen && (
         <div className={css.burgerMenu}>
           <button
@@ -79,8 +131,9 @@ export default function Header() {
           </button>
           <div className={css.menuContent}>
             <Nav isHome={false} />
+
             <div className={css.menuAuth}>
-          {isAuthenticated ? <UserNav /> : <AuthNav />}
+          {isAuthenticated ? <LogOutBtn/> : <AuthNav />}
             </div>
             </div>
           </div>

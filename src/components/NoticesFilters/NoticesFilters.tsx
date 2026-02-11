deae49b7ebe32542742cf2d5bfd06c38e1892541
ShumyloYana <zolotushko1@gmail.com>
2026-02-11 12:42:45 +0200

    Add filtering functionality to Notices page

--- src/components/NoticesFilters/NoticesFilters.tsx
+++ src/components/NoticesFilters/NoticesFilters.tsx
@@ -1,39 +1,228 @@
-// import { useMutation, useQueryClient } from '@tanstack/react-query';
-// import { useState } from 'react';
-// import { useForm } from 'react-hook-form';
-// import { yupResolver } from '@hookform/resolvers/yup';
-// import * as Yup from 'yup';
-// import { AxiosError } from 'axios';
-// import toast from 'react-hot-toast';
+import { useState } from "react";
+import AsyncSelect from "react-select/async";
+import { components } from "react-select";
+import { searchCities } from "../../api/citiesApi";
+import SearchField from "../SearchField/SearchField";
+import type { Category, Species, Sex } from "../../types/notices";
+import type { City } from "../../types/city";
+import css from "./NoticesFilters.module.css";
 
-// interface NoticesFormValues {
+interface NoticesFiltersProps {
+  params: {
+    keyword?: string;
+    category?: Category;
+    species?: Species;
+    sex?: Sex;
+    locationId?: string;
+    byPopularity?: boolean;
+    byPrice?: boolean;
+  };
 
-// }
+  categories: Category[];
+  species: Species[];
+  sexes: Sex[];
 
-// const validationSchema = Yup.object({
-// keyword: Yup.string()
-//     .min(3, 'Keyword must be at least 3 characters')
-//     .max(48, 'Keyword a long')
-//         .required('Keywordis required'),
-// });
-    
-// export default function NoticesFilters() {
-//     return (
-//     <form
-//       className={css.form}
-//       onSubmit={handleSubmit(onSubmit)}
-//       noValidate
-//       autoComplete="off"
-//     >
-  
-//         <div className={css.inputWrapper}>
-//           <input
-//             type="text"
-//             placeholder="keyword" value={keyword}
+  onChange: (next: NoticesFiltersProps["params"]) => void;
+}
 
-//           />
-//         </div>
-//             <p className={css.error}>{errors.name?.message}</p>
-            
-            
-//       </form>
+export default function NoticesFilters({
+  params,
+  categories,
+  species,
+  sexes,
+  onChange,
+}: NoticesFiltersProps) {
+  const [selectedCity, setSelectedCity] = useState<City | null>(null);
+
+  const handleReset = () => {
+    setSelectedCity(null);
+    onChange({});
+  };
+
+  return (
+    <section className={css.filters}>
+    <div className={css.wrapperFilters} >
+      <SearchField
+        onSubmit={(keyword) =>
+          onChange({ ...params, keyword: keyword || undefined })
+        }
+        placeholder="Search"
+        />
+              
+        <div className={css.wrapperCategoriaBygender} >
+          <div className={css.selectwrapper}>
+      <select className={css.select}
+        value={params.category ?? ""}
+        onChange={(e) =>
+          onChange({
+            ...params,
+            category: (e.target.value as Category) || undefined,
+          })
+        }
+      >
+        <option value="">Category</option>
+        {categories.map((c) => (
+          <option key={c} value={c}>
+            {c}
+          </option>
+        ))}              
+            </select>
+            </div>
+
+        <div className={css.selectwrapper}>
+      <select className={css.select}
+        value={params.sex ?? ""}
+        onChange={(e) =>
+          onChange({
+            ...params,
+            sex: (e.target.value as Sex) || undefined,
+          })
+        }
+      >
+        <option value="">By gender</option>
+        {sexes.map((s) => (
+          <option key={s} value={s}>
+            {s}
+          </option>
+        ))}
+        </select>
+        </div>
+        </div>
+
+        <div className={css.selectwrapper}>
+      <select className={`${css.selectSpecies} ${css.selectType}`}
+        value={params.species ?? ""}
+        onChange={(e) =>
+          onChange({
+            ...params,
+            species: (e.target.value as Species) || undefined,
+          })
+        }
+      >
+        <option value="">By type</option>
+        {species.map((s) => (
+          <option key={s} value={s}>
+            {s}
+          </option>
+        ))}
+          </select>
+        </div>
+      
+        <AsyncSelect<City, false>
+  className={css.asyncSelectContainer}
+  classNamePrefix="react-select"
+  cacheOptions
+  defaultOptions={false}
+  loadOptions={async (inputValue) => {
+    if (inputValue.length < 3) return [];
+    return await searchCities(inputValue);
+  }}
+  value={selectedCity}
+  onChange={(option) => {
+    setSelectedCity(option);
+    onChange({
+      ...params,
+      locationId: option?._id,
+    });
+  }}
+  getOptionLabel={(o) => `${o.cityEn}, ${o.stateEn}`}
+  getOptionValue={(o) => o._id}
+  placeholder="Location"
+  isClearable
+  noOptionsMessage={({ inputValue }) =>
+    inputValue.length < 3 ? "Type at least 3 characters" : "No cities found"
+  }
+  components={{
+    DropdownIndicator: (props) => (
+      <components.DropdownIndicator {...props}>
+        <svg width={18} height={18}>
+          <use href="/svg-sprite.svg#icon-search" />
+        </svg>
+      </components.DropdownIndicator>
+    ),
+    ClearIndicator: (props) => (
+      <components.ClearIndicator {...props}>
+        <svg width={18} height={18}>
+          <use href="/svg-sprite.svg#icon-cross-small" />
+        </svg>
+      </components.ClearIndicator>
+    ),
+  }}
+/>
+        </div>
+
+      {/* сортування за популярністю та ціною */}
+<div className={css.sort}>
+
+  <label>
+    <input
+      type="radio"
+      name="sort"
+      checked={params.byPopularity === false} 
+      onChange={() =>
+        onChange({
+          ...params,
+          byPopularity: false, 
+          byPrice: undefined,
+        })
+      }
+    />
+    Popular
+  </label>
+
+
+  <label>
+    <input
+      type="radio"
+      name="sort"
+      checked={params.byPopularity === true}
+      onChange={() =>
+        onChange({
+          ...params,
+          byPopularity: true, 
+          byPrice: undefined,
+        })
+      }
+    />
+    Unpopular
+  </label>
+
+  <label>
+    <input
+      type="radio"
+      name="sort"
+      checked={params.byPrice === true}
+      onChange={() =>
+        onChange({
+          ...params,
+          byPrice: true, 
+          byPopularity: undefined,
+        })
+      }
+    />
+    Cheap
+  </label>
+
+  <label>
+    <input
+      type="radio"
+      name="sort"
+      checked={params.byPrice === false}
+      onChange={() =>
+        onChange({
+          ...params,
+          byPrice: false, 
+          byPopularity: undefined,
+        })
+      }
+    />
+    Expensive
+  </label>
+</div>
+
+      <button type="button" onClick={handleReset} className={css.reset}>
+        Reset
+      </button>
+    </section>
+  );
+}
--- src/pages/NoticesPage/NoticesPage.tsx
+++ src/pages/NoticesPage/NoticesPage.tsx
@@ -2,9 +2,32 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import NoticesList from "../../components/NoticesList/NoticesList";
 import Title from "../../components/Title/Title";
-import {getNotices} from "../../api/noticesApi";
+import { getNotices } from "../../api/noticesApi";
+import NoticesFilters from "../../components/NoticesFilters/NoticesFilters";
+import type { Category, Species, Sex } from "../../types/notices";
 import type { GetNoticesParams } from "../../api/noticesApi";
 import css from "./NoticesPage.module.css";
+
+const categories: Category[] = ["sell", "free", "lost", "found"];
+
+const species: Species[] = [
+  "dog",
+  "cat",
+  "monkey",
+  "bird",
+  "snake",
+  "turtle",
+  "lizard",
+  "frog",
+  "fish",
+  "ants",
+  "bees",
+  "butterfly",
+  "spider",
+  "scorpion",
+];
+
+const sexes: Sex[] = ["unknown", "female", "male", "multiple"];
 
 export default function NoticesPage() {
   const [params, setParams] = useState<GetNoticesParams>({});
@@ -15,9 +38,25 @@ export default function NoticesPage() {
     placeholderData: (prev) => prev,
   });
 
+    const handleFiltersChange = (next: Partial<GetNoticesParams> | {}) => {
+    if (next && Object.keys(next).length > 0) {
+      setParams((prev) => ({ ...prev, ...next, page: 1 }));
+    } else {
+      setParams({});
+    }
+    };
+  
   return (
     <section className={css.NoticesPage}>
-          <Title title="Find your favorite pet" />
+      <Title className={css.titleNoticesPage} title="Find your favorite pet" />
+      
+      <NoticesFilters
+        params={params}
+        categories={categories}
+        species={species}
+        sexes={sexes}
+        onChange={handleFiltersChange}
+      />
           
       {data && <NoticesList items={data.results} />}
     </section>

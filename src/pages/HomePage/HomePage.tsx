f1d7761835de5752228ead0c922e40f6b2d40303
ShumyloYana <zolotushko1@gmail.com>
2026-02-21 12:05:35 +0200

    Fix style

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,6 +8,6 @@ export default function HomePage() {
             <h1 className={css.title} >Take good <span className={css.textSpan}>care</span> of your small pets</h1>
             <p className={css.text}>Choosing a pet for your home is a choice that is meant to enrich your life with immeasurable joy and tenderness.</p>
             </div>
-            <img className={css.image}src="/home.webp" srcSet="/home.webp 375w, /home@2x.webp 768w, /home@3x.webp 1280w" alt="Image a young woman with a dog" loading="lazy"/>
+            <img className={css.image}src="/home.webp" srcSet="/home.webp, /home@2x.webp, /home@3x.webp" alt="Image a young woman with a dog" loading="lazy"/>
         </section>)
 }
